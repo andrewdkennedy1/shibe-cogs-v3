a94d68a11b43e510ed9bde905a3c2feee04c96dc
andrewdkennedy1 <andrew@bortonfruit.com>
2020-05-12 13:14:07 -0700

    still idk regex

--- move/move.py
+++ move/move.py
@@ -14,10 +14,10 @@ class move(commands.Cog):
     async def move(self, ctx):
         """[p]move [messageID] [channelID]"""
 
-        content = ctx.message.content
+        content = re.search('\.move\s*(\d+)\s*to\s*<#(\d+)>\s*', ctx.message.content)
 
         if content == None:
-            await ctx.send(f'{ctx.message.author.mention}: The command specified is incorrectly formatted - (bp!move <message id> <channel>)', delete_after=5)
+            await ctx.send(f'{ctx.message.author.mention}: The command specified is incorrectly formatted - (.move <message id> <channel>)', delete_after=5)
             return
 
         message_id = int(content.group(1))
