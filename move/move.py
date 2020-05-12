d1e38198a0d0c7dff9a6d22172bb4b3715ad1aa9
andrewdkennedy1 <andrew@bortonfruit.com>
2020-05-12 12:55:37 -0700

    indents matter

--- move/move.py
+++ move/move.py
@@ -12,12 +12,12 @@ class move(commands.Cog):
     @commands.command()
     async def move(self, ctx):
         """[p]move [messageID] [channelID]"""
-                msg = "**Message from** {} ({})\n".format(
-                    message.channel.mention, message.channel.id
-                )
-                msg = "**Message sent by**: {} ({})\n".format(
-                    message.author.name, message.author.id
-                )
-                msg += "**Message content**:\n{}".format(message.content)
-                await message_channel.send(msg)
-                await message.delete()
+        msg = "**Message from** {} ({})\n".format(
+            message.channel.mention, message.channel.id
+        )
+        msg = "**Message sent by**: {} ({})\n".format(
+            message.author.name, message.author.id
+        )
+        msg += "**Message content**:\n{}".format(message.content)
+        await message_channel.send(msg)
+        await message.delete()
