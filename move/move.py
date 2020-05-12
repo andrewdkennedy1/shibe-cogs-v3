2b9b41de8aea02e21661eb7a484b3d98a4cba4c5
andrewdkennedy1 <andrew@bortonfruit.com>
2020-05-12 12:54:02 -0700

    polly pocket

--- move/move.py
+++ move/move.py
@@ -1,12 +1,23 @@
+import asyncio
+
+import discord
+
 from redbot.core import commands
 
 
 # Classname should be CamelCase and the same spelling as the folder
 class move(commands.Cog):
-    """[p]move [messageID] [channelID]"""
+    """Take your message here and move it over there"""
 
     @commands.command()
     async def move(self, ctx):
         """[p]move [messageID] [channelID]"""
-        # Your code will go here
-        await ctx.send("I like to move it , move it!")
+                msg = "**Message from** {} ({})\n".format(
+                    message.channel.mention, message.channel.id
+                )
+                msg = "**Message sent by**: {} ({})\n".format(
+                    message.author.name, message.author.id
+                )
+                msg += "**Message content**:\n{}".format(message.content)
+                await message_channel.send(msg)
+                await message.delete()
