31d8cdb3dfcfb14e5f3b0743429e4243156f0388
andrewdkennedy1 <andrew@bortonfruit.com>
2020-05-17 05:59:01 -0700

    .content

--- move/move.py
+++ move/move.py
@@ -25,7 +25,7 @@ class move(commands.Cog):
             return
         files = []
         for attachment in target_message.attachments:
-            files.append(discord.File(fp=io.BytesIO(requests.get(attachment.url)).content, filename=attachment.filename, spoiler=attachment.is_spoiler()))
+            files.append(discord.File(fp=io.BytesIO(requests.get(attachment.url).content), filename=attachment.filename, spoiler=attachment.is_spoiler()))
 
         embed = discord.Embed(title='Message moved from: %s' % (target_message.channel), description='', color=0x00ff00)
         embed.set_author(name=target_message.author.name, url=target_message.author.avatar_url, icon_url=target_message.author.avatar_url)
