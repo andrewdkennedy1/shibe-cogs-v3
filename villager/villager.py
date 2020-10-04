b02df416c9ee277ea9af51b1fd4f5d5902b0b2cb
andrewdkennedy1 <andrew@bortonfruit.com>
2020-10-04 16:10:13 -0700

    .

--- villager/villager.py
+++ villager/villager.py
@@ -10,16 +10,12 @@ class villager(commands.Cog):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.__url: str = "https://nookipedia.com/wiki/"
-        self.__session = aiohttp.ClientSession()
-
     def cog_unload(self) -> None:
         if self.__session:
             asyncio.get_event_loop().create_task(self.__session.close())
 
     @commands.command()
-    async def villager(self, ctx):
+    async def villager(self, ctx, villager):
         """villager"""
         await ctx.trigger_typing()
-        await ctx.send(self.__url+ctx)
-        
+        await ctx.send("https://nookipedia.com/wiki/" + villager)
