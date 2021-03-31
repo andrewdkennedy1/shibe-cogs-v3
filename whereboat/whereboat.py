23a339c4f9ced520855c5b11aa4a780237743ffb
shibe <andrewdkennedy1@gmail.com>
2021-03-30 23:08:50 -0700

    afasdf

--- whereboat/whereboat.py
+++ whereboat/whereboat.py
@@ -23,5 +23,5 @@ class whereboat(commands.Cog):
         url = self.base_url
         async with self.session.get(url, headers=self.header) as response:
             soup = BeautifulSoup(await response.text(), "html.parser")
-        data = soup.find.all('h3')
+        data = soup.find_all('h3')
         await ctx.send(data)
