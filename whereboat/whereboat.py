00c8516c90d08b7721bd1475b63e11a5108ae243
shibe <andrewdkennedy1@gmail.com>
2021-05-06 21:12:09 -0700

    yee

--- whereboat/whereboat.py
+++ whereboat/whereboat.py
@@ -12,7 +12,7 @@ class whereboat(commands.Cog):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.base_url = "http://aaucargo.info"
+        self.base_url = "https://archeage.mokulu.io/schedule"
         self.session = aiohttp.ClientSession()
         self.header = {"User-Agent": "ShibeCogs/3.0"}
 
@@ -23,9 +23,7 @@ class whereboat(commands.Cog):
         url = self.base_url
         async with self.session.get(url, headers=self.header) as response:
             soup = BeautifulSoup(await response.text(), "html.parser")
-        data = soup.find_all('h3')
-        status = data[0].text
-        leavetime = data[1].text
-        nextport = data[2].text
-        em=discord.Embed(title="WHERE BOAT!?", url="http://aaucargo.info/", description=status + " \n " + leavetime , color=0xFF5733)
+        data = soup.find_all("div", {"class": "MuiPaper-root cargo-ship event-list MuiPaper-elevation1 MuiPaper-rounded"})
+        status = data
+        em=discord.Embed(title="WHERE BOAT!?", url="https://archeage.mokulu.io/schedule", description=status)
         await ctx.send(embed=em)
