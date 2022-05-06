1c807d6cd7428a104f4f19fbd4f3f1f783085db9
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2022-05-06 14:10:16 -0700

    init

---
+++ status/status.py
@@ -0,0 +1,16 @@
+import asyncio
+import re
+import io
+import discord
+import requests
+from datetime import datetime
+from redbot.core import commands
+
+
+# Classname should be CamelCase and the same spelling as the folder
+class status(commands.Cog):
+    """gib status pls"""
+
+    @commands.command(pass_context=True)
+    async def status(ctx, member: Member):
+    await bot.say(str(member.status))
