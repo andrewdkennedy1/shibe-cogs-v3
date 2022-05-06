1a0dfbbe63de250c6d628fdaf6ac98ef08ed853b
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2022-05-06 16:10:57 -0700

    Update status.py

--- status/status.py
+++ status/status.py
@@ -17,20 +17,33 @@ class status(BaseCog):
         if not member:
             member = author
 
+        if any(a.type is discord.ActivityType.streaming for a in member.activities):
+            statusemoji = "\N{LARGE PURPLE CIRCLE}"
+        elif member.status.name == "online":
+            statusemoji = "\N{LARGE GREEN CIRCLE}"
+        elif member.status.name == "offline":
+            statusemoji = "\N{MEDIUM WHITE CIRCLE}\N{VARIATION SELECTOR-16}"
+        elif member.status.name == "dnd":
+            statusemoji = "\N{LARGE RED CIRCLE}"
+        elif member.status.name == "idle":
+            statusemoji = "\N{LARGE ORANGE CIRCLE}"
+        activity = ("Chilling in {} status").format(member.status)
+
         status_string = "No Custom Status Dectected"
 
         for s in member.activities:
             if isinstance(s, discord.CustomActivity):
                 status_string = s
-                
+
         name = str(member)
-        name = " ~ ".join((name, member.nick)) if member.nick else name
+        name = (name, member.nick) if member.nick else name
 
         embed = discord.Embed()
         embed.title = name
         embed.colour = member.colour
-        embed.set_footer(text="Powered by ShibeBot!")
+        embed.add_field(name="What Doing?", value=activity)
         embed.add_field(name="Custom Status", value=status_string)
         embed.set_thumbnail(url=member.avatar_url_as(static_format="png"))
+        embed.set_footer(text="Powered by ShibeBot!")
 
         await ctx.send(embed=embed)
