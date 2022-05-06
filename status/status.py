e54bb2f6dce547bab7dfee66e090b0c26fcf052b
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2022-05-06 15:15:01 -0700

    Update status.py

--- status/status.py
+++ status/status.py
@@ -37,9 +37,9 @@ class status(commands.Cog):
         activity = _("Chilling in {} status").format(member.status)
 
         for s in member.activities:
-        if isinstance(s, discord.CustomActivity):
+            if isinstance(s, discord.CustomActivity):
             status_string = s
-    
+
         data = discord.Embed(description=status_string or activity, colour=member.colour)
         name = str(member)
         name = " ~ ".join((name, member.nick)) if member.nick else name
