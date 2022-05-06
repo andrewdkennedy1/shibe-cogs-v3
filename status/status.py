9968ebac38e472f95fa2d222af1ecb9b78cece0d
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2022-05-06 14:32:50 -0700

    .

--- status/status.py
+++ status/status.py
@@ -13,20 +13,43 @@ class status(commands.Cog):
 
     @commands.command(pass_context=True)
     async def status(self, ctx, *, member: discord.Member = None):
-        """Show a member's status"""
+        """gib status"""
         author = ctx.author
         guild = ctx.guild
+
         if not member:
             member = author
+
+        #  A special case for a special someone :^)
+        special_date = datetime.datetime(2016, 1, 10, 6, 8, 4, 443000, datetime.timezone.utc)
+        is_special = member.id == 96130341705637888 and guild.id == 133049272517001216
+
+        roles = member.roles[-1:0:-1]
+        names, nicks = await self.get_names_and_nicks(member)
+
+        if is_special:
+            joined_at = special_date
+        else:
+            joined_at = member.joined_at
+        voice_state = member.voice
         member_number = (
             sorted(guild.members, key=lambda m: m.joined_at or ctx.message.created_at).index(
                 member
             )
             + 1
         )
-        
-        names, nicks = await self.get_names_and_nicks(member)
 
+        created_on = (
+            f"{discord.utils.format_dt(member.created_at)}\n"
+            f"{discord.utils.format_dt(member.created_at, 'R')}"
+        )
+        if joined_at is not None:
+            joined_on = (
+                f"{discord.utils.format_dt(joined_at)}\n"
+                f"{discord.utils.format_dt(joined_at, 'R')}"
+            )
+        else:
+            joined_on = _("Unknown")
 
         if any(a.type is discord.ActivityType.streaming for a in member.activities):
             statusemoji = "\N{LARGE PURPLE CIRCLE}"
@@ -40,11 +63,49 @@ class status(commands.Cog):
             statusemoji = "\N{LARGE ORANGE CIRCLE}"
         activity = _("Chilling in {} status").format(member.status)
         status_string = self.get_status_string(member)
+
+        if roles:
+            role_str = ", ".join([x.mention for x in roles])
+            # 400 BAD REQUEST (error code: 50035): Invalid Form Body
+            # In embed.fields.2.value: Must be 1024 or fewer in length.
+            if len(role_str) > 1024:
+                # Alternative string building time.
+                # This is not the most optimal, but if you're hitting this, you are losing more time
+                # to every single check running on users than the occasional user info invoke
+                # We don't start by building this way, since the number of times we hit this should be
+                # infinitesimally small compared to when we don't across all uses of Red.
+                continuation_string = _(
+                    "and {numeric_number} more roles not displayed due to embed limits."
+                )
+                available_length = 1024 - len(continuation_string)  # do not attempt to tweak, i18n
+
+                role_chunks = []
+                remaining_roles = 0
+
+                for r in roles:
+                    chunk = f"{r.mention}, "
+                    chunk_size = len(chunk)
+
+                    if chunk_size < available_length:
+                        available_length -= chunk_size
+                        role_chunks.append(chunk)
+                    else:
+                        remaining_roles += 1
+
+                role_chunks.append(continuation_string.format(numeric_number=remaining_roles))
+
+                role_str = "".join(role_chunks)
+
+        else:
+            role_str = None
+
         data = discord.Embed(description=status_string or activity, colour=member.colour)
         data.set_footer(text=_("Member #{} | User ID: {}").format(member_number, member.id))
+
         name = str(member)
         name = " ~ ".join((name, member.nick)) if member.nick else name
         name = filter_invites(name)
+
         avatar = member.display_avatar.replace(static_format="png")
         data.set_author(name=f"{statusemoji} {name}", url=avatar)
         data.set_thumbnail(url=avatar)
