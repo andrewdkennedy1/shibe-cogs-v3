41aa476c22d381355f29c74f8c8b790e582b1eca
Andrew <34286315+andrewdkennedy1@users.noreply.github.com>
2022-05-06 15:45:14 -0700

    Update status.py

--- status/status.py
+++ status/status.py
@@ -1,7 +1,7 @@
-
 import discord
-
-from redbot.core import commands
+from redbot.core import commands, Config, checks
+import aiohttp
+from redbot.core.utils.menus import menu, commands, DEFAULT_CONTROLS
 
 BaseCog = getattr(commands, "Cog", object)
 
