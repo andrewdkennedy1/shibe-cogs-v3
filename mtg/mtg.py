2edcd7ab5dab552fc5400fb8336e933d8f080ba1
therealshibe <andrewdkennedy1@gmail.com>
2022-10-10 11:12:52 -0700

    i wait

--- mtg/mtg.py
+++ mtg/mtg.py
@@ -14,7 +14,32 @@ class mtg(commands.Cog):
         """
         session = aiohttp.ClientSession()
 
-
         resp = await session.get(url='http://api.scryfall.com/cards/named?', params={'fuzzy':cardname})
         card = await resp.json()
-        await ctx.send(card)
+
+        if card['object'] == "error":
+            await ctx.send(re.sub(r'\(|\'|,|\)+', '', card['details']))
+            return
+
+        if 'card_faces' in card:
+            for entry in card['card_faces']:
+                message = discord.Embed(
+                    title="**{}**".format(entry['name']),
+                    url=card['scryfall_uri'],
+                    color=discord.Color(0x1b6f9)
+                )
+                message.set_image(url=entry['image_uris']['normal'])
+                message.set_footer(text="Fetch took: {} seconds.".format('%.3f' % f))
+                await ctx.send(embed=message)
+            return
+
+        message = discord.Embed(
+            title="**{}**".format(card['name']),
+            url=card['scryfall_uri'],
+            color=discord.Color(0x1b6f9),
+            description=""
+        )
+
+        message.set_footer(text="Fetch took: {} seconds.".format('%.3f' % f))
+        message.set_image(url=card['image_uris']['normal'])
+        await ctx.send(embed=message)
