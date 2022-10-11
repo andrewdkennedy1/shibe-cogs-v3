7a1ed755f64b75fb75c98e4cba9fc7cb6eac6c70
therealshibe <andrewdkennedy1@gmail.com>
2022-10-10 22:01:38 -0700

    rats

--- mtg/mtg.py
+++ mtg/mtg.py
@@ -6,6 +6,7 @@ class mtg(A.Cog):
 		E=D.ClientSession();F=await E.get(url='http://api.scryfall.com/cards/named?',params={'fuzzy':cardname});A=await F.json()
 		if A['object']=='error':await ctx.send(re.sub("\\(|\\'|,|\\)+",'',A['details']));return
 		C=B.Embed(title='**{}**'.format(A['name']),url=A['scryfall_uri']);C.set_image(url=A['image_uris']['normal']);await ctx.send(embed=C)
+	@A.command(pass_context=True)
 	async def mtgrandom(self,ctx,*,cardname):
 		E=D.ClientSession();F=await E.get(url='http://api.scryfall.com/cards/random?',params={'q':cardname});A=await F.json()
 		if A['object']=='error':await ctx.send(re.sub("\\(|\\'|,|\\)+",'',A['details']));return
